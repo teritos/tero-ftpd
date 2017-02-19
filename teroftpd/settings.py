0da4730a2b960898ec43db967e342f4041fa1f9a
Emiliano Dalla Verde Marcozzi <emiliano@devsar.com>
2017-02-19 18:22:42 -0300

    Tomo la configuracion de la base de datos, del ftp y de redis desde variables de entorno (issue #3)

--- teroftpd/settings.py
+++ teroftpd/settings.py
@@ -5,28 +5,27 @@ import pathlib
 
 HOST = os.getenv('TERO_FTP_HOST', '0.0.0.0')
 PORT = os.getenv('TERO_FTP_PORT', 2121)
-confdir = str(pathlib.Path.home() / pathlib.Path('.config/tero'))
-ROOTDIR = os.getenv('TERO_FTP_ROOTDIR', confdir)
-SECRET_KEY = 'secret'
-REDIS_HOST = os.getenv('REDIS_HOST', '0.0.0.0')
-POSTGRESQL_HOST = os.getenv('POSTGRESQL_HOST', '0.0.0.0')
+PASSIVE_PORTS_MIN = os.getenv('PASSIVE_PORTS_MIN')
+PASSIVE_PORTS_MAX = os.getenv('PASSIVE_PORTS_MAX')
+ROOTDIR = os.getenv('TERO_FTP_FILES_DIR') or str(pathlib.Path.home() / pathlib.Path('.config/tero'))
+
+SECRET_KEY = os.getenv('DJANGO_SECRET') or 'secret'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tero',
-        'USER': 'postgres',
-        'PASSWORD': 'tero',
-        'HOST': POSTGRESQL_HOST,
+        'NAME': os.getenv('TERO_PGSQL_NAME'),
+        'USER': os.getenv('TERO_PGSQL_USER'),
+        'HOST': os.getenv('TERO_PGSQL_HOST'),
+        'PORT': os.getenv('TERO_PGSQL_PORT'),
+        'PASSWORD': os.getenv('TERO_PGSQL_PASSWORD'),
     }
 }
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(REDIS_HOST, 6379)],
+            "hosts": [(os.getenv('TERO_REDIS_HOST'), os.getenv('TERO_REDIS_PORT'))],
         },
         "ROUTING": "teroftpd.routing.channel_routing",
     },
 }
-PASSIVE_PORTS_MIN = 30000
-PASSIVE_PORTS_MAX = 31000
